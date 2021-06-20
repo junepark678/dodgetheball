7635ecdd9768d25ce03ab6415dda0a182eca3ed2
junepark678 <rjp2030@outlook.com>
2021-06-20 20:50:49 +0900

    modify and create requirements.txt

--- dodgetheball/settings.py
+++ dodgetheball/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-wdc!v^ojy617w36_qrs*zo5j$s##$fye^-*l=*crf&o@g)u#f='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [".herokuapp.com", "127.0.0.1:8000"]
 
